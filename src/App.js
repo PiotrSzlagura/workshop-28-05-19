e5ff5370e880431fb664a091837c9f16102137f4
Piotr Szlagura <piotr.szlagura@ageno.pl>
2019-05-28 16:38:57 +0200

    Initial commit with some code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getText() {
+    return "Kot ma Ale";
+}
+
+const SomeWrapper = ({children}) => (
+    children
+);
+
+class App extends React.Component {
+    componentDidMount() {
+        this.listElement.addEventListener("click", (event) => {
+            console.log(event)
+        })
+    }
+
+    render() {
+        const element2 = <span>Some other text</span>
+
+        const element = (
+            <React.Fragment>
+                <div>{element2} {getText()}</div>
+                <div>{element2} {getText()}</div>
+            </React.Fragment>
+        );
+
+        return (
+            <div className="App">
+                <ul ref={(element) => {
+                    this.listElement = element;
+                }} onClick={(event) => {
+                    console.log(event)
+                }}>
+                    {["element1", "element2"].map((element, index) => (
+                        <SomeWrapper key={index}>
+                            <li>{element}</li>
+                            <li>{element}</li>
+                        </SomeWrapper>
+                    ))}
+                </ul>
+                {element}
+                <button onClick={() => {
+                    this.setState({
+                        value: Math.random()
+                    })
+                }}>click me
+                </button>
+            </div>
+        );
+    }
 }
 
 export default App;
